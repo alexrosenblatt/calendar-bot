d8cccaf21cf355de8f50ba4f9cf8ec8c09969410
alexrosenblatt <rosenblatt.alex@gmail.com>
2022-12-05 20:38:28 -0800

    Initial check-in of Alex's calendar bot with boiler plate code

---
+++ zulip_bots/zulip_bots/bots/calendar-bot/calendarbot.py
@@ -0,0 +1,12 @@
+class CalendarBotHandler(object):
+    '''
+    A docstring documenting this bot.
+    '''
+
+    def usage(self):
+        return '''Your description of the bot'''
+
+    def handle_message(self, message, bot_handler):
+        # add your code here
+
+handler_class = MyBotHandler
