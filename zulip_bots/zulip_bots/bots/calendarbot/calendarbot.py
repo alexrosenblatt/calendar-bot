57b437c1884709c2fde6b11a79f6d8242781a9d6
alexrosenblatt <rosenblatt.alex@gmail.com>
2022-12-07 12:02:49 -0800

    Initial checkin of branch

--- zulip_bots/zulip_bots/bots/calendarbot/calendarbot.py
+++ zulip_bots/zulip_bots/bots/calendarbot/calendarbot.py
@@ -22,50 +22,57 @@ class CalendarBotHandler(object):
         return """Your description of the bot"""
 
     def handle_message(self, message:dict, bot_handler):
-        cal = Calendar()
         message_content = message['content'].lower()
+        cal = Calendar()
         
         #casematch is overkill for now, but i suspect future cases to be added
-        #splits meet command and datetime input
         match message_content.split():
-            case ['meet',datetime_input,*ignore]:
+            case ['meet',datetime_input,length_minutes]:         
+                try:
+                    length_minutes = int(length_minutes)
+                    meeting_details = self.parse_meeting_details(message,datetime_input)
+                    self.create_calendar_event(meeting_details,cal)
+                    self.send_event_file(bot_handler,cal, message)
+                except:
+                    self.input_error_reply(message,bot_handler)
+            case ['meet',datetime_input]: #TODO add error handling here
                 meeting_details = self.parse_meeting_details(message,datetime_input)
                 self.create_calendar_event(meeting_details,cal)
-                self.send_event_file(bot_handler, cal, message)
+                self.send_event_file(bot_handler,cal, message)
             case _:
-                self.input_error(message,bot_handler)
+                self.input_error_reply(message,bot_handler)
 
 
-    def input_error(self,message: dict,bot_handler):
+    def input_error_reply(self,message: dict,bot_handler):
         logging.error(f"User entered:{message['content']} - parsing failed")
-        response = "Sorry - I didn't understand that. Please use syntax 'Meet <time'"
+        response = "Sorry - I didn't understand that. Please use syntax 'Meet [<time:] [duration in minutes, default is 30] '"
         bot_handler.send_reply(message, response)
 
-    def parse_meeting_details(self,message,datetime_input) -> tuple[int,list,datetime,int,dict]:
+    def parse_meeting_details(self,message,datetime_input,length_minutes=30) -> tuple[int,list,datetime,int,dict]:
         
         #converts Zulip global timepicker into a python datetime object
         meeting_datetime = datetime.fromisoformat(datetime_input.replace('<time:','').replace('>',''))
         sender_id = message['sender_id']
-        
-        meeting_length = 30 #TODO make this selectable
+    
 
         # get recipients from message json and remove sender and bot
         recipients = list(map(lambda recipient: (recipient['id'],recipient['email']) , message['display_recipient']))
         invitees = [recipient[1] for recipient in recipients if (recipient[1] != bot_email)]
         
-        return sender_id,invitees,meeting_datetime,meeting_length,message
+        return sender_id,invitees,meeting_datetime,length_minutes,message
     
     def create_calendar_event(self,meeting_details: tuple[int,list,datetime,int,dict],cal):    
         
         #todo maybe convert the below to named_tuples or a dataclass?
-        sender_id,invitees,meeting_datetime,meeting_length,message = meeting_details
+        sender_id,invitees,meeting_datetime,length_minutes,message = meeting_details
         
         #append meeting data to Event object
+   
         event = Event()
         event.name = "Fun meeting"
         event.description = "Wowzers - this meeting is prettttty dope."
         event.begin = meeting_datetime
-        event.end = meeting_datetime+timedelta(minutes=meeting_length)
+        event.end = meeting_datetime+timedelta(minutes=int(length_minutes))
         
         #create attendee list
         for invitee_email in invitees:
@@ -83,16 +90,20 @@ class CalendarBotHandler(object):
         #     print(bot_handler.upload_file(my_file))
     
 
-    def send_event_file(self, bot_handler, cal, message):
+    def send_event_file(self, bot_handler,cal, message):
         with open('my.ics', 'r+') as my_file:
             my_file.writelines(cal.serialize_iter())
+            my_file.close()
 
-        #reopening due to some race condition that affect upload files
         with open('my.ics', 'r+') as my_file:    
             result = bot_handler.upload_file(my_file)
             response = f"[Meeting Invite]({result['uri']})."
             bot_handler.send_reply(message,response)
+            
+            #erase file before close
+            my_file.truncate(0)
             my_file.close()
+        
           
 
 handler_class = CalendarBotHandler
---
+++ zulip_bots/zulip_bots/bots/calendarbot/googlecalendar.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+from google.oauth2 import service_account
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+SERVICE_ACCOUNT_FILE = "python-zulip-api/zulip_bots/zulip_bots/bots/calendarbot/creds.json"
+
+
+def main():
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
+
+    try:
+        service = build("calendar", "v3", credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        print("Getting the upcoming 10 events")
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=10,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
+
+        if not events:
+            print("No upcoming events found.")
+            return
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
+
+    except HttpError as error:
+        print("An error occurred: %s" % error)
+
+
+if __name__ == "__main__":
+    main()
