3e6af2eb1feffde3121eb673561d38cff50c8884
alexrosenblatt <rosenblatt.alex@gmail.com>
2022-12-08 11:30:39 -0800

    Almost done with PR refactor

--- zulip_bots/zulip_bots/bots/calendarbot/calendarbot.py
+++ zulip_bots/zulip_bots/bots/calendarbot/calendarbot.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from datetime import date, datetime, time, timedelta
 from tempfile import TemporaryFile
 from time import sleep
+from re import search
 
 import zulip_bots.bots.calendarbot.googlecalendar as gcal
 from ics import Attendee, Calendar, Event
@@ -12,7 +13,7 @@ from zulip_bots.lib import BotHandler
 logging.basicConfig(filename='calendarbot.log', encoding='utf-8', level=logging.DEBUG)
 
 #TODO: Take this from environment variables in the future
-bot_email = 'calendarbot2-bot@recurse.zulipchat.com'
+BOT_REGEX = '.+(bot@).+(zulipchat.com)'
 
 #create calendar_object
 
@@ -37,46 +38,49 @@ class CalendarBotHandler(object):
 
 
     def usage(self):
-        return """Your description of the bot"""
+        return """Creates calendar meeting for all participants on chat."""
 
     def handle_message(self, message:dict, bot_handler):
         self.message = message
         self.bot_handler = bot_handler
         message_content = message['content'].lower()
+        
+        #instantiating Calendar() here for now due to bug when trying to move it to init, works but may cause issues in future
         cal = Calendar()
 
         
         #casematch is overkill for now, but i suspect future cases to be added
         match message_content.split():
-            case ['meet',datetime_input,length_minutes]:         
-                try:
-                    length_minutes = int(length_minutes)
-                    meeting_details = self.parse_meeting_details(datetime_input)
-                    self.create_calendar_event(meeting_details,cal)
-                    self.send_event_file(cal)
-                except:
-                    self.input_error_reply()
-            case ['meet',datetime_input]: #TODO add error handling here
-                meeting_details = self.parse_meeting_details(datetime_input)
-                self.create_calendar_event(meeting_details,cal)
-                self.send_google_event_invite(meeting_details)
-                self.send_event_file(cal)
+            case [datetime_input,length_minutes]:         
+                logging.debug(f"Datetime_input:{datetime_input},length_minutes {length_minutes}")
+                self.create_and_send_meeting(cal, datetime_input, length_minutes)
+            case [datetime_input]: 
+                logging.debug(f"Datetime_input:{datetime_input}")
+                self.create_and_send_meeting(cal, datetime_input)
             case _:
                 self.input_error_reply()
+
+    def create_and_send_meeting(self, cal, datetime_input, length_minutes=30):
+        try:
+            meeting_details = self.parse_meeting_details(datetime_input,length_minutes)
+            self.create_calendar_event(meeting_details,cal)
+            self.send_event_file(cal)
+        except:
+             self.input_error_reply()
 
     def send_google_event_invite(self,meeting_details):
         gcal.send_google_invite(meeting_details)
         
     def input_error_reply(self):
         logging.error(f"User entered:{self.message['content']} - parsing failed")
-        response = "Sorry - I didn't understand that. Please use syntax 'Meet [<time:] [duration in minutes, default is 30] '"
+        response = "Sorry - I didn't understand that. Please use syntax '[<time:] [duration in minutes, default is 30] '"
         self.bot_handler.send_reply(self.message,response)
 
-    def parse_meeting_details(self,datetime_input,length_minutes=30) -> MeetingDetails:
+    def parse_meeting_details(self,datetime_input,length_minutes) -> MeetingDetails:
+        
         #converts Zulip global timepicker into a python datetime object
-        print(datetime_input)
-        meeting_start = datetime.fromisoformat(datetime_input.replace('<time:','').replace('>',''))
-        meeting_end = meeting_start+timedelta(minutes=int(length_minutes))
+        meeting_start, meeting_end = self.parse_datetime_input(datetime_input, length_minutes)
+        
         name = "test name"
         summary = "test summary"
         sender_id = self.message['sender_id']
@@ -84,7 +88,7 @@ class CalendarBotHandler(object):
 
         # get recipients from message json and remove sender and bot
         recipients = list(map(lambda recipient: (recipient['id'],recipient['email']) , self.message['display_recipient']))
-        invitees = [recipient[1] for recipient in recipients if (recipient[1] != bot_email)]
+        invitees = [recipient[1] for recipient in recipients if (search(BOT_REGEX,recipient[1]) != True)]
         
         meeting = self.MeetingDetails(sender_id=sender_id,
                 invitees=invitees,
@@ -94,6 +98,11 @@ class CalendarBotHandler(object):
                 name=name,summary=summary)
 
         return meeting
+
+    def parse_datetime_input(self, datetime_input, length_minutes) -> tuple[datetime, datetime]:
+        meeting_start = datetime.fromisoformat(datetime_input.replace('<time:','').replace('>',''))
+        meeting_end = meeting_start+timedelta(minutes=int(length_minutes))
+        return meeting_start,meeting_end
     
     def create_calendar_event(self,meeting: MeetingDetails,cal):    
         
