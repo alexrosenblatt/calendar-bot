684db8bbadc334c8b9802043b6848a97a9e7fe8f
mwen00 <melanie.a.wen@gmail.com>
2022-12-14 13:30:24 -0800

    cleanup parsing logic and get ready for merge

--- zulip_bots/zulip_bots/bots/calendar_bot/calendar_bot.py
+++ zulip_bots/zulip_bots/bots/calendar_bot/calendar_bot.py
@@ -30,29 +30,43 @@ class CalEvent:
 
 @dataclass
 class CalendarHandler:
-    # event: CalEvent = field(default_factory=CalEvent())
-    # cal: Calendar = field(default_factory=Calendar())
-
     def usage(self) -> str:
         return (
             "Calendar Bot is a meeting scheduler. Enter a Zulip global time `<time` and then a duration in minutes (e.g. 60 for 1 hour). Default event duration is 30 mins and the default email addresses are the emails associated with user emails. There is an option to change invitees with a comma separated list of email addresses."
         )
 
+    def initialize(self, bot_handler: BotHandler) -> None:
+        storage = bot_handler.storage
+
+        # Initialize the key values in the bot_handler storage and allows the parsing logic to be a little cleaner
+        if not storage.contains("confirmation"):
+            storage.put("confirmation", None)
+        if not storage.contains("datetime"):
+            storage.put("datetime", None)
+        if not storage.contains("custom_duration"):
+            storage.put("custom_duration", None)
     
-    def parse_first_response(self, message: Dict[str, Any], bot_handler: BotHandler) -> str:
-        bot_handler.storage.put("Confirmation", None)
-        # Split the initial message string to CalendarBot
+    
+    def message_content_helper(self, message: Dict[str, Any]) -> List[str]:
+        # Split the message string to CalendarBot
         args = message["content"].lower().split()
+
         # Filter out any arguments that could be related to the bot name. This occurs when there are more than 2 users in the chat.
-        filtered_args = [x for x in args if not re.search(BOT_NAME, x)]
+        return [x for x in args if not re.search(BOT_NAME, x)]
+
+    
+    def parse_first_message(self, message: Dict[str, Any], bot_handler: BotHandler) -> str:
+        bot_handler.storage.put("Confirmation", None)
+        
+        filtered_args = self.message_content_helper(message)
         num_args = len(filtered_args)
         duration = DEFAULT_CALEVENT_DURATION
 
-        # Typechecking for Zulip global time and optional duration argument
+        # Validate args Zulip global time and duration
         if not num_args:
             return self.usage()
         elif num_args > 2: 
-            return f"TypeError: Expected at most 2 arguments, received {num_args}"
+            return f"Expected at most 2 arguments, received {num_args}"
         
         if not re.search(TIME_FORMAT, filtered_args[0]):
             return f"Could not parse {message['content']}. Please pass in a Zulip global time `<time`"
@@ -60,40 +74,66 @@ class CalendarHandler:
         try:
             if num_args == 2:
                 duration = int(filtered_args[1])
-                bot_handler.storage.put("duration", duration)
+                bot_handler.storage.put("custom_duration", duration)
         except:
             return f"Could not parse duration input {filtered_args[1]}"
 
         time = filtered_args[0].replace("<time:", "").replace(">", "")
         bot_handler.storage.put("datetime", time)
 
-        confirm_message = f"Create a meeting at {filtered_args[0]} for {duration}mins? (Y / N)"
+        confirm_message = f"Create a meeting at {filtered_args[0]} for {duration} mins? (Y / N)"
         
         return confirm_message
     
 
-    def parse_second_response(message: Dict[str, Any], bot_handler: BotHandler) -> None:
-        ... # Y/N in message["content"]
-        return 
+    def parse_second_message(self, message: Dict[str, Any], bot_handler: BotHandler) -> None:
+        filtered_args = self.message_content_helper(message)
+        confirmation = filtered_args[0]
+
+        if confirmation == "y":
+            self.create_event(bot_handler)
+            return "Success!"
+        elif confirmation == "n":
+            self.clear_storage(bot_handler)
+            return "Got it! I will remove any saved event data."
+        else:
+            raise TypeError
+
+    
+    def clear_storage(self, bot_handler: BotHandler) -> None:
+        bot_handler.storage.put("confirmation", None)
+        bot_handler.storage.put("datetime", None)
+        bot_handler.storage.put("duration", None)
+        
+        return
+
+    def create_event(self, bot_handler: BotHandler) -> None:
+        # Remove data from storage
+        self.clear_storage(bot_handler)
+
+        return
 
 
     def handle_message(self, message: Dict[str, Any], bot_handler: BotHandler) -> None:
         storage = bot_handler.storage
 
+        # TODO: Remove after testing
         print(bot_handler.storage.get("confirmation"))
         print(bot_handler.storage.get("datetime"))
 
         # Parse the initial message containing the event time and duration info
-        if storage.get("confirmation") is None and not storage.contains("datetime"):
-            response = self.parse_first_response(message, bot_handler)
-            bot_handler.send_reply(message, response)
+        if storage.get("confirmation") is None and storage.get("datetime") is None:
+            bot_response = self.parse_first_message(message, bot_handler)
+            bot_handler.send_reply(message, bot_response)
+
+        # Parse the second message for the meeting info confirmation
         elif storage.get("confirmation") is None and storage.contains("datetime"):
-            # self.parse_second_response(message, bot_handler)
-            ...
-        elif storage.get("confirmation") == "Y":
-            ... # trigger calevent stuff and then wipe out the storage
-        elif storage.get("confirmation") == "N":
-            ... # send reply, OK, won't create this! Send self.usage()
-            ... # wipe out everything in storage
+            try:
+                bot_response = self.parse_second_message(message, bot_handler)
+            except: 
+                bot_response = f"Could not parse confirmation message. Please message CalendarBot with 'Y' / 'N' to confirm <time:{storage.get('datetime')}>"
+            
+            bot_handler.send_reply(message, bot_response)
+
 
 handler_class = CalendarHandler
